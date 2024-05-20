e3c77dc34706c8853bc884731c257bf7ba09dd0e
Yon Wayne <chandler.taylor15@gmail.com>
2024-05-20 15:05:39 +0000

    feat: still working on api again

--- public/script.js
+++ public/script.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const urlParams = new URLSearchParams(window.location.search);
   const dateParam = urlParams.get("date");
 
-  const apiUrl = `https://localhost:3000/api/${dateParam}`;
+  // Construct the API URL based on the date parameter
+  const apiUrl = `http://localhost:3000/api/${dateParam}`;
 
+  // Fetch data from the API
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
       return response.json();
     })
     .then((data) => {
+      // Update the HTML content with the received data
       document.getElementById("current-date").innerHTML =
         "Unix Timestamp: " + data.unix + "<br>UTC: " + data.utc;
       console.log(data.unix);
@@ -20,5 +23,4 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => {
       console.error("Error:", error);
     });
-  console.log("JavaScript file loaded correctly.");
 });
